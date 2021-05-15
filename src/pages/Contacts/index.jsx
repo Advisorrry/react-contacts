4f3869ca1cff0b302a39bf02a738c67d8f07b6a1
advisorrry <sypikeh@yandex.com>
2021-05-15 13:19:34 +0500

    added filter by nationality and added clear filter button

--- src/pages/Contacts/index.jsx
+++ src/pages/Contacts/index.jsx
@@ -1,17 +1,6 @@
 import React from 'react'
 
-import {
-    Box,
-    CircularProgress,
-    Container,
-    FormControl,
-    Grid,
-    InputLabel,
-    MenuItem,
-    Select,
-    TextField,
-    Typography,
-} from '@material-ui/core'
+import { Box, CircularProgress, Container, Grid, Typography } from '@material-ui/core'
 import { makeStyles, createStyles } from '@material-ui/core/styles'
 
 import { useContacts } from '../../hooks/useContacts'
@@ -19,6 +8,7 @@ import { ContactTable } from './ContactTable'
 import { ToggleViewMode } from '../../components/ToggleViewMode'
 import { DATA_VIEW_MODES } from '../../constants/data_view_modes'
 import { useViewMode } from '../../hooks/useViewMode'
+import { ContactsFilters } from '../ContactsFilters'
 
 const useStyles = makeStyles((theme) =>
     createStyles({
@@ -28,16 +18,13 @@ const useStyles = makeStyles((theme) =>
         headContainer: {
             marginBottom: theme.spacing(3),
         },
-        filtersContainer: {},
-        formControl: {
-            minWidth: 120,
-        },
     }),
 )
 
 const filtersDefaultValue = {
     fullname: '',
-    gender: '',
+    gender: 'all',
+    nationality: 'all',
 }
 
 export const Contacts = () => {
@@ -52,10 +39,10 @@ export const Contacts = () => {
         localStorage.setItem('viewMode', viewMode)
     }, [viewMode])
 
-    const handleChangeFilter = (e) => {
+    const updateFilters = (name, value) => {
         setFilters((prevFilter) => ({
             ...prevFilter,
-            [e.target.name]: e.target.value,
+            [name]: value,
         }))
     }
 
@@ -66,13 +53,21 @@ export const Contacts = () => {
         )
     }
     const filterByGender = (gender, filterGender) => {
-        if (filterGender.length === 0) return true
+        if (filterGender === 'all') return true
         return gender === filterGender
+    }
+    const filterByNationality = (nationality, filterNationality) => {
+        if (filterNationality === 'all') return true
+        return nationality === filterNationality
+    }
+    const clearFilters = () => {
+        setFilters(filtersDefaultValue)
     }
 
     const filteredContacts = contacts.data
         .filter((c) => filterByFullname(c.name, filters.fullname))
         .filter((c) => filterByGender(c.gender, filters.gender))
+        .filter((c) => filterByNationality(c.nat, filters.nationality))
 
     return (
         <Container className={classes.root}>
@@ -85,33 +80,7 @@ export const Contacts = () => {
                         <ToggleViewMode viewMode={viewMode} setViewMode={setViewMode} />
                     </Box>
                 </Grid>
-                <Grid item xs={12} className={classes.filtersContainer}>
-                    <Box display="flex">
-                        <TextField
-                            label="Search by full name"
-                            variant="outlined"
-                            size="small"
-                            name="fullname"
-                            value={filters.fullname}
-                            onChange={handleChangeFilter}
-                        />
-                        <FormControl
-                            variant="outlined"
-                            size="small"
-                            className={classes.formControl}>
-                            <InputLabel id="gender">Gender</InputLabel>
-                            <Select
-                                onChange={handleChangeFilter}
-                                labelId="gender"
-                                name="gender"
-                                value={filters.gender}>
-                                <MenuItem value={''}>All</MenuItem>
-                                <MenuItem value={'male'}>Male</MenuItem>
-                                <MenuItem value={'female'}>Female</MenuItem>
-                            </Select>
-                        </FormControl>
-                    </Box>
-                </Grid>
+                <ContactsFilters filters={filters} updateFilters={updateFilters} clearFilters={clearFilters} />
                 <Grid item xs={12}>
                     {(() => {
                         if (contacts.isLoading) {
